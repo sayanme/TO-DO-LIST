98ed969c61022f6d3b96965559019313fbbb76ac
Sayan Mitra <sayanmitra061@gmail.com>
2025-09-08 17:50:13 +0530

    index.js

--- index.js
+++ index.js
@@ -1,57 +1,12 @@
-/*function addtask() {
-    const newtask = document.createElement("li");
-    const tasklist = document.getElementById('Task-list');
-    addTaskoption(newtask)
-    tasklist.appendChild(newtask);
-    newtask.textContent = document.getElementById('task').value;
-    document.getElementById('task').value = "";
-    deletetask(newtask)
-    
-}
-function deletetask(newtask){
-
-const deletebtn=document.createElement('button');
-deletebtn.textContent="Delete"
-newtask.appendChild(deletebtn);
-deletebtn.onclick =function(){
-    newtask.remove()
-}}
-function addTaskoption(newtask) {
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.style.marginRight = "10px";
-    newtask.appendChild(checkbox);
-
-    checkbox.onchange = function () {
-        if (checkbox.checked) {
-            newtask.style.textDecoration = "line-through";
-        } else {
-            newtask.style.textDecoration = "none";
-        }
-    }
-}*/
-
-
-
 function addtask() {
     const newtask = document.createElement("li");
     const tasklist = document.getElementById('Task-list');
-
-    // Add checkbox first
     addTaskoption(newtask);
-
-    // Add text inside a span (so checkbox is not removed)
     const taskText = document.createElement("span");
     taskText.textContent = document.getElementById('task').value;
     newtask.appendChild(taskText);
-
-    // Clear input
     document.getElementById('task').value = "";
-
-    // Add delete button
     deletetask(newtask);
-
-    // Append li to ul
     tasklist.appendChild(newtask);
 }
 
@@ -80,4 +35,5 @@ function addTaskoption(newtask) {
         }
     }
 }
+
 
