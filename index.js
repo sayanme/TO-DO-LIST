d3afc469ced9abe6f6357fd6ecff3390138b6bb9
Sayan Mitra <sayanmitra061@gmail.com>
2025-09-19 16:48:11 +0530

    Add files via upload
    after modified the files

---
+++ index.js
@@ -0,0 +1,38 @@
+function addtask() {
+    const newtask = document.createElement("li");
+    const tasklist = document.getElementById('Task-list')
+    addTaskoption(newtask);
+    const taskText = document.createElement("span");
+    taskText.textContent = document.getElementById('task').value;
+    newtask.appendChild(taskText);
+    document.getElementById('task').value = "";
+    deletetask(newtask);
+    tasklist.appendChild(newtask);
+}
+
+function deletetask(newtask) {
+    const deletebtn = document.createElement('button');
+    deletebtn.textContent = "Delete";
+    deletebtn.style.marginLeft = "10px";
+    newtask.appendChild(deletebtn);
+
+    deletebtn.onclick = function () {
+        newtask.remove();
+    }
+}
+
+function addTaskoption(newtask) {
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.style.marginRight = "10px";
+    newtask.appendChild(checkbox);
+
+    checkbox.onchange = function () {
+        if (checkbox.checked) {
+            newtask.style.textDecoration = "line-through";
+        } else {
+            newtask.style.textDecoration = "none";
+        }
+    }
+}
+
